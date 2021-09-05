06e06ee5afda745122f9c4b3205e4b68a5ccb043
bilelrahmouni <bilelrahmouni01@hotmail.fr>
2021-09-05 22:55:50 +0300

    add photo and post with description to firebase storage

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import rootReducer from "./redux/reducers";
 import thunk from "redux-thunk";
 import MainScreen from "./components/Main";
 import AddScreen from "./components/main/Add";
+import SaveScreen from "./components/main/Save";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 const Stack = createNativeStackNavigator();
@@ -74,7 +75,16 @@ export class App extends Component {
               component={MainScreen}
               options={{ headerShown: false }}
             />
-            <Stack.Screen name="Add" component={AddScreen} />
+            <Stack.Screen
+              name="Add"
+              component={AddScreen}
+              navigation={this.props.navigation}
+            />
+            <Stack.Screen
+              name="Save"
+              component={SaveScreen}
+              navigation={this.props.navigation}
+            />
           </Stack.Navigator>
         </NavigationContainer>
       </Provider>
